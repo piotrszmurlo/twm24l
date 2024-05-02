8c1d9bcd9ac6d9be193ee56d83ede96b5bb72043
Piotr Szmurlo <pszmurlo@box.com>
2024-05-02 09:58:29 +0200

    ssd stuff

--- utils.py
+++ utils.py
@@ -1,5 +1,5 @@
 from torch import Tensor, uint8
-
+import torch
 
 def collate_fn(batch):
     return tuple(zip(*batch))
@@ -13,7 +13,6 @@ def to_absolute_bbox_format(bbox: list[float], img_width: int, img_height: int) 
 
 
 def train_one_epoch(model, optimizer, data_loader, epoch):
-    model.train()
     print(f"-------------- EPOCH {epoch} ---------------")
     for images, targets in data_loader:
         targets = [{k: v for k, v in t.items()} for t in targets]
@@ -29,3 +28,12 @@ def train_one_epoch(model, optimizer, data_loader, epoch):
 def rescale_image(image: Tensor):
     test_image_rescaled = (255.0 * (image - image.min()) / (image.max() - image.min())).to(uint8)
     return test_image_rescaled
+
+def add_to_metric_fn(metric_fn, predictions, targets):
+    rows = len(targets[0]['boxes'])
+    pred_rows = len(predictions[0]['boxes'])
+    targets_extended = torch.column_stack((targets[0]['boxes'], torch.zeros((rows, 3))))
+    predictions_extended = torch.column_stack((predictions[0]['boxes'], torch.zeros((pred_rows, 1))))
+    pkk = predictions[0]['scores'].reshape(-1,1)
+    predictions_extended = torch.column_stack((predictions_extended, pkk))
+    metric_fn.add(predictions_extended.numpy(), targets_extended.numpy())
